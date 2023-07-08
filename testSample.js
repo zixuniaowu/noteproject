0f0235f0bb1ec496dc56d04a2cba828e7bb51e15
jacky wang <shenghuawang666@gmail.com>
2023-07-08 20:26:15 +0800

    aborts

---
+++ testSample.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+    if (req.method == 'POST') {
+        var url_parse = url.parse(req.url, true);
+        console.log(url_parse);
+        res.end();
+    }else if(req.method == 'GET') {
+      console.log("hello word GET ");
+    }else if(req.method == 'PUT') {
+      console.log("hello word PUT");
+    }else if(req.method == 'DELETE') {
+      console.log("hello word DEL");
+    }
+
+}).listen(8080);
